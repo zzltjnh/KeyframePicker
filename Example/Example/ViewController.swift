19aa705a776f82808dc78e176a3607ec06e8fe40
zhangzhilong <zhangzhilong@breadtrip.com>
2016-11-10 18:41:08 +0800

    create

---
+++ Example/Example/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by zhangzhilong on 2016/11/10.
+//  Copyright © 2016年 zhangzhilong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ KeyframePicker/KeyframePicker/KeyframePicker.h
@@ -0,0 +1,19 @@
+//
+//  KeyframePicker.h
+//  KeyframePicker
+//
+//  Created by zhangzhilong on 2016/11/10.
+//  Copyright © 2016年 zhangzhilong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KeyframePicker.
+FOUNDATION_EXPORT double KeyframePickerVersionNumber;
+
+//! Project version string for KeyframePicker.
+FOUNDATION_EXPORT const unsigned char KeyframePickerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KeyframePicker/PublicHeader.h>
+
+
